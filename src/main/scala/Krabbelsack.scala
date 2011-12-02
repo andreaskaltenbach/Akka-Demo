c4adca758d95a7bf7a431a8aeb4cac95ade0c348
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2011-12-02 08:49:37 +0100

    actors implemented with correct case class messages

--- src/main/scala/Krabbelsack.scala
+++ src/main/scala/Krabbelsack.scala
@@ -2,6 +2,9 @@ package se.stendahls.krabbelsack
 
 import akka.actor.Actor.actorOf
 import akka.actor.{ActorRef, Actor}
+import collection.mutable.{DoubleLinkedList, Queue}
+import java.util.Random
+import javax.print.attribute.standard.PresentationDirection
 
 object Application extends App {
 
@@ -10,43 +13,38 @@ object Application extends App {
   val krabbelsackActor = actorOf[KrabbelsackActor]
   krabbelsackActor.start
 
-  val erik = actorOf(new PersonActor("Erik", new Beer, krabbelsackActor))
-  val matsola = actorOf(new PersonActor("Mats-Ola", new Honey, krabbelsackActor))
-  val daniel = actorOf(new PersonActor("Daniel", new Volleyball, krabbelsackActor))
-  val anders = actorOf(new PersonActor("Anders", new Chicken, krabbelsackActor))
+  val erik = actorOf(new PersonActor("Erik", Beer, 1000, krabbelsackActor))
+  val matsola = actorOf(new PersonActor("Mats-Ola", Honey, 1000, krabbelsackActor))
+  val daniel = actorOf(new PersonActor("Daniel", Volleyball, 60000, krabbelsackActor))
+  val anders = actorOf(new PersonActor("Anders", Chicken, 45000, krabbelsackActor))
   Set(erik, matsola, daniel, anders).foreach(_.start)
 
-  val begin = new Begin(krabbelsack)
+  val begin = new Start(krabbelsack)
   Set(erik, matsola, daniel, anders).foreach(_ ! begin)
-
-
-
-
-
-  //krabbelsackActor ! new SwitchPresent(new Beer)
-  Set(krabbelsackActor, erik, matsola, daniel, anders).foreach(_.stop)
 }
 
-// switch message
-case class Begin(krabbelsack:Krabbelsack)
-case class SwitchPresent(present:Present)
+// messages
+case class Start(krabbelsack:Krabbelsack)
 
-trait Present
-class Beer extends Present
-class Honey extends Present
-class Volleyball extends Present
-class Chicken extends Present
+case class Present(name:String)
 
+object Beer extends Present("Beer")
+object Honey extends Present("Honey")
+object Volleyball extends Present("Volleyball")
+object Chicken extends Present("Chicken")
 
-
-
-
-class PersonActor(name:String, present:Present, krabbelsackActor:ActorRef) extends Actor {
+class PersonActor(name:String, initialPresent:Present, waitingTime:Long, krabbelsackActor:ActorRef) extends Actor {
 
   def receive = {
-    case Begin(krabbelsack) => {
-      println(name + " is putting his present " + present + " into the krabbelsack.")
-      krabbelsackActor ! SwitchPresent(present)
+    case Start(krabbelsack) => {
+      println(name + " is putting his present " + initialPresent + " into the krabbelsack.")
+      krabbelsackActor ! initialPresent
+    }
+    case Present(p) => {
+      println(name + " got a " + p + " as present.")
+      Thread.sleep(waitingTime)
+      println(name + " does not want his " + p + " and returns it to the krabbelsack.")
+      krabbelsackActor ! Present(p)
     }
   }
 }
@@ -58,17 +56,32 @@ class Krabbelsack {
 
 class KrabbelsackActor extends Actor {
 
-  val switchPresentMessages:Set[SwitchPresent] = Set()
+  var personQueue:Queue[ActorRef] = Queue()
+  var presents:Vector[ScalaObject] = Vector()
 
   def receive = {
 
-    case SwitchPresent(s) => {
-      println("Switch request" + s)
+    case Present(p) => {
 
-      self.sender
-      self reply "asf"
+      println("Adding " + p + " to the krabbelsack.")
+      personQueue += self.sender.get
+      presents = Present(p) +: presents
+      println("Krabbelsack now contains " + presents.size + " presents.")
+      println("------------------------------------")
+
+      while(presents.size > 1) {
+
+        // fetch random present
+        val randomIndex = new Random().nextInt(presents.size)
+        val present = presents(randomIndex)
+        presents = presents filter (_ == present)
+
+        // give present to next person in queue
+        val personActor = personQueue.dequeue
+        personActor ! present
+      }
     }
-
-
   }
 }
+
+case class EmployeeWish(previousPresent:Present)
